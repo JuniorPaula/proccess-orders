8ef844ca7a541ce7818244f8e21bb166ff314460
Junior Paula <luke.junnior@icloud.com>
2023-01-30 17:55:02 -0300

    Ensure test given a valid params when call new order then should receive create order with all params

--- internal/order/entity/order_test.go
+++ internal/order/entity/order_test.go
@@ -20,3 +20,15 @@ func TestGivenAnEmptyTax_WhenCreateANewOrder_ThenShouldReceiveAnError(t *testing
 	order := Order{ID: "123", Price: 10}
 	assert.Error(t, order.IsValid(), "invalid tax")
 }
+
+func TestGivenAValidParams_WhenCallNewOrder_ThenShouldReceiveCreateOrderWithAllParams(t *testing.T) {
+	order := Order{
+		ID:    "123",
+		Price: 100.0,
+		Tax:   2.0,
+	}
+	assert.Equal(t, "123", order.ID)
+	assert.Equal(t, 100.0, order.Price)
+	assert.Equal(t, 2.0, order.Tax)
+	assert.Nil(t, order.IsValid())
+}
